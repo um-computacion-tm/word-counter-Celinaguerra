b9e16ec546e6f2d0ee8e8c211d3ea89ef33417c7
Celinaguerra <celinaguerradiaz@gmail.com>
2023-04-09 18:30:30 -0300

    cuenta palabras repetidas-TERMINADO

--- cont_palabras.py
+++ cont_palabras.py
@@ -1,26 +1,10 @@
-import unittest
 def contpalabras(palabra):
-    numpalabras = len(palabra.split())
-    return {palabra:numpalabras}
+    dic_palabras = {}
+    palabra = palabra.split()
+    for pal in palabra:
+        if pal in dic_palabras:
+            dic_palabras[pal]=dic_palabras[pal]+1
+        else:
+            dic_palabras[pal]=1
 
-class TestPalabras(unittest.TestCase):
-
-    def test1(self):
-        rdo = contpalabras("hola")
-        self.assertEqual(rdo,{"hola":1})
-    
-    def test2 (self):
-        rdo=contpalabras("buenas tardes")
-        self.assertEqual(rdo,{"buenas tardes":2})
-
-    def test3 (self):
-        rdo=contpalabras("habia una vez un lobo muy pequeño")
-        self.assertEqual(rdo,{"habia una vez un lobo muy pequeño":7})
-
-    def test4 (self):
-        rdo=contpalabras("buenas buenas, como va todo?")
-        self.assertEqual(rdo,{"buenas buenas, como va todo?":5})
-
-
-if __name__=="__main__":
-    unittest.main()
+    return dic_palabras
